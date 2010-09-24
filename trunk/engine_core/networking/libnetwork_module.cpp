01dddff1426414d8af0d054a24dda7bd7ee44fcb
alexsuh4 <alexsuh4@07f38270-9850-d47c-b3d9-8ccd393251a1>
2010-09-24 14:32:18 +0000

    socket working on windows , pproblem with gamelet.dll for some reason .

--- trunk/engine_core/networking/libnetwork_module.cpp
+++ trunk/engine_core/networking/libnetwork_module.cpp
@@ -1,6 +1,3 @@
-
-
-
 #include <cstring>
 #include <iostream>
 #include <string>
--- trunk/portability/posix_Sockets_windows.h
+++ trunk/portability/posix_Sockets_windows.h
@@ -1,33 +1,16 @@
 #ifndef _POSIX_SOCKETS_WINDOWS_H
+#ifdef _MSC_VER
 #define _POSIX_SOCKETS_WINDOWS_H
-/**
-this file acts as a thin layer on top of win32 function to port
-a tiny subset of posix socket functions into win32 enviroment
-*/
-#include <windows.h>
-#include <sys/types.h>
-#include <io.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <share.h>
 
 #define ssize_t unsigned int
 #define socklen_t int
-//ssize_t write(int fildes, const void *buf, size_t nbyte)
-//{
-//	return _write(fildes,buf,nbyte);
-//}
-//ssize_t read(int fildes, void *buf, size_t nbyte)
-//{
-//	return _read(filedes,buf,nbytes);
-//}
-//int close( int fd )
-//{
-//	return _close(fd );
-//}
 
+int read(int fd, void *buf, size_t nbyte);
+ssize_t write(int fildes, const void *buf, size_t nbyte);
+int close(int fildes);
+
+#endif
 #endif
