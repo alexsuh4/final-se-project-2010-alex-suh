645cf6bffe00fc073518b2b7259102f205dd4533
alexsuh4 <alexsuh4@07f38270-9850-d47c-b3d9-8ccd393251a1>
2010-10-22 15:35:35 +0000

    map1.xml

---
+++ gamelets/flashGamelet/posix_for_windows.h
@@ -0,0 +1,19 @@
+/***
+code for simple pthread_mutex operetions for windows
+*/
+#ifndef _PTHREAD_MUTEX_FOR_WINDOWS
+#define _PTHREAD_MUTEX_FOR_WINDOWS
+#include <windows.h>
+typedef HANDLE pthread_mutex_t; 
+void pthread_mutex_init(pthread_mutex_t *mutex_hanlde,int * ignorable_var);
+void pthread_mutex_destroy(pthread_mutex_t *mutex_hanlde);
+DWORD pthread_mutex_lock(pthread_mutex_t *mutex_handle);
+BOOL pthread_mutex_unlock(pthread_mutex_t *mutex_handle);
+
+
+
+
+
+
+
+#endif
